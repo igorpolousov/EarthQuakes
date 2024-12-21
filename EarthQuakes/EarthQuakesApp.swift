cb3665bbdb24067284900cd62f0c0281db44177d
Igor Polousov <igorpolousov@gmail.com>
2024-12-21 14:51:40 +0300

    Initial Commit

---
+++ EarthQuakes/EarthQuakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthQuakesApp.swift
+//  EarthQuakes
+//
+//  Created by Igor Polousov on 21.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct EarthQuakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
