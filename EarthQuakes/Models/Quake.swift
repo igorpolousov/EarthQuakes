9a119e7b42645ffa1320ea526679ad76779311dd
Igor Polousov <igorpolousov@gmail.com>
2024-12-26 15:53:34 +0300

    + quake error with description + readme comments

--- EarthQuakes/Models/Quake.swift
+++ EarthQuakes/Models/Quake.swift
@@ -24,3 +24,48 @@ struct Quake {
 extension Quake: Identifiable {
     var id: String {code} // как еще можно использовать такой указатель типа переменной?
 }
+
+extension Quake: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        case magnitude = "mag"
+        case place
+        case time
+        case code
+        case detail
+    }
+    
+    init(from decoder: any Decoder) throws {
+        // code below was provided by Apple's tutorial
+        //        let container = try decoder.container(keyedBy: CodingKeys.self)
+        //        self.magnitude = try container.decode(Double.self, forKey: .magnitude)
+        //        self.place = try container.decode(String.self, forKey: .place)
+        //        self.time = try container.decode(Date.self, forKey: .time)
+        //        self.code = try container.decode(String.self, forKey: .code)
+        //        self.detail = try container.decode(URL.self, forKey: .detail)
+        
+        
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let rawMagnitude = try? values.decode(Double.self, forKey: .magnitude)
+        let rawPlace = try? values.decode(String.self, forKey: .place)
+        let rawTime = try? values.decode(Date.self, forKey: .time)
+        let rawCode = try? values.decode(String.self, forKey: .code)
+        let rawDetail = try? values.decode(URL.self, forKey: .detail)
+        
+        guard let magnitude = rawMagnitude,
+              let place = rawPlace,
+              let time = rawTime,
+              let code = rawCode,
+              let detail = rawDetail
+        else {
+            throw QuakeError.missingData
+        }
+        
+        self.magnitude = magnitude
+        self.place = place
+        self.time = time
+        self.code = code
+        self.detail = detail
+        
+    }
+}
--- EarthQuakes/Models/QuakeError.swift
+++ EarthQuakes/Models/QuakeError.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+enum QuakeError: Error {
+    case missingData
+}
+
+extension QuakeError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .missingData:
+            return NSLocalizedString("Found and will discard a quake missing a valid code, magnitude, place, or time.", comment: "")
+        }
+    }
+}
