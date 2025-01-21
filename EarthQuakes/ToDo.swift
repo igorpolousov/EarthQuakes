d44ec28346045a930d41420b159485e5b21422ac
Igor Polousov <igorpolousov@gmail.com>
2025-01-21 16:16:50 +0300

    + ToDo.swift файл для напоминания на предстоящие задачи

---
+++ EarthQuakes/ToDo.swift
@@ -0,0 +1,11 @@
+//
+//  ToDo.swift
+//  EarthQuakes
+//
+//  Created by Igor Polousov on 21.01.2025.
+//
+// Файл напоминание
+
+/*
+ https://developer.apple.com/tutorials/app-dev-training/building-a-network-test-client
+ */
