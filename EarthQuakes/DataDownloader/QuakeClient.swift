bbf229d8627106984c828aead715a23204d1eca8
Igor Polousov <igorpolousov@gmail.com>
2025-01-30 14:29:04 +0300

    + cashing qauke location to quakeclient

--- EarthQuakes/DataDownloader/QuakeClient.swift
+++ EarthQuakes/DataDownloader/QuakeClient.swift
@@ -7,10 +7,9 @@
 
 import Foundation
 
-class QuakeClient {
+actor QuakeClient {
     
     private let quakeCashe: NSCache<NSString, CashEntryObject> = NSCache()
-    
     
     var quakes: [Quake] {
         get async throws {
@@ -33,4 +32,33 @@ class QuakeClient {
     init(downloader: any HTTPDataDownloader = URLSession.shared) {
         self.downloader = downloader
     }
+    
+    func quakeLocation(from url: URL) async throws -> QuakeLocation {
+        
+        if let cashed = quakeCashe[url] {
+            switch cashed {
+            case .ready(let location) :
+                return location
+            case .inProgress(let task):
+                return try await task.value
+            }
+        }
+        
+        let task = Task<QuakeLocation, Error> {
+            let data = try await downloader.httpData(from: url)
+            let location = try decoder.decode(QuakeLocation.self, from: data)
+            return location
+        }
+        quakeCashe[url] = .inProgress(task)
+        do {
+            let location = try await task.value
+            quakeCashe[url] = .ready(location)
+            return location
+        } catch {
+            quakeCashe[url] = nil
+            throw error
+        }
+        
+    }
+  
 }
