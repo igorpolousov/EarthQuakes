1cc23c4383cc04eec4190a5773682cc4073d77e1
Igor Polousov <igorpolousov@gmail.com>
2025-02-11 13:22:46 +0300

    + navigation link from quakes to quake detail

--- EarthQuakes/Views/Quakes.swift
+++ EarthQuakes/Views/Quakes.swift
@@ -40,7 +40,10 @@ struct Quakes: View {
         NavigationView {
             List(selection: $selection) {
                 ForEach(provider.quakes) { quake in
-                    QuakeRow(quake: quake)
+                    NavigationLink(destination: QuakeDetail(quake: quake)) {
+                        QuakeRow(quake: quake)
+                    }
+
                 }
                 .onDelete(perform: deleteQuakes)
             }
