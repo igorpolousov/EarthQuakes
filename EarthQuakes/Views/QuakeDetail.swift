53b89fd43afa67180dffbf952c16d2f87e325b26
Igor Polousov <igorpolousov@gmail.com>
2025-02-11 14:37:43 +0300

    + precision change on onTapGesture 2 options in code

--- EarthQuakes/Views/QuakeDetail.swift
+++ EarthQuakes/Views/QuakeDetail.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct QuakeDetail: View {
     
     var quake: Quake
+    @State private var onTapLongLatt: Bool = true
     var body: some View {
         VStack {
             QuakeMagnitude(quake: quake)
@@ -20,8 +21,38 @@ struct QuakeDetail: View {
                 .foregroundStyle(Color.secondary)
             
             if let location = quake.location {
-                Text("Latitude: \(location.latitude.formatted(.number.precision(.fractionLength(3))))")
-                Text("Longitude: \(location.longitude.formatted(.number.precision(.fractionLength(3))))")
+                // Вариант 1
+                VStack {
+                    Text(!onTapLongLatt ? "Longitude: \(location.latitude)" : "Latitude: \(location.latitude.formatted(.number.precision(.fractionLength(3))))" )
+                    
+                    Text(!onTapLongLatt ? "Longitude: \(location.longitude)" : "Latitude: \(location.longitude.formatted(.number.precision(.fractionLength(3))))" )
+                }
+                .onTapGesture {
+                    onTapLongLatt.toggle()
+                }
+                
+                //Вариант 2
+//                if onTapLongLatt == true {
+//                    VStack {
+//                        Text("Latitude: \(location.latitude.formatted(.number.precision(.fractionLength(3))))")
+//                        
+//                        Text("Longitude: \(location.longitude.formatted(.number.precision(.fractionLength(3))))")
+//                    }
+//                    .onTapGesture {
+//                        onTapLongLatt.toggle()
+//                    }
+//                } else {
+//                    VStack {
+//                        Text("Latitude: \(location.latitude)")
+//                        
+//                        Text("Longitude: \(location.longitude)")
+//                          
+//                    }
+//                    .onTapGesture {
+//                        onTapLongLatt.toggle()
+//                    }
+//                       
+//                }
             }
         }
     }
